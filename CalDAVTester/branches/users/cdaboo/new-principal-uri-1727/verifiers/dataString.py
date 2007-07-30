76722249897cda222404d6520085d52627085fe0
cdaboo@apple.com <cdaboo@apple.com@e27351fd-9f3e-4f54-a53b-843176b1656c>
2007-07-30 17:35:50 +0000

    Branch to sync up with changes to calendar server's principal-URI.
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver@1728 e27351fd-9f3e-4f54-a53b-843176b1656c

---
+++ CalDAVTester/branches/users/cdaboo/new-principal-uri-1727/verifiers/dataString.py
@@ -0,0 +1,47 @@
+##
+# Copyright (c) 2006-2007 Apple Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# DRI: Cyrus Daboo, cdaboo@apple.com
+##
+
+"""
+Verifier that checks the response body for an exact match to data in a file.
+"""
+
+class Verifier(object):
+    
+    def verify(self, manager, uri, response, respdata, args): #@UnusedVariable
+        # Get arguments
+        contains = args.get("contains", [])
+        notcontains = args.get("notcontains", [])
+        
+        # status code must be 200, 207
+        if response.status not in (200,207):
+            return False, "        HTTP Status Code Wrong: %d" % (response.status,)
+        
+        # look for response data
+        if not respdata:
+            return False, "        No response body"
+        
+        # Check each contains and not-contains (AND operation)
+        for item in contains:
+            if respdata.find(item.replace("\n", "\r\n")) == -1:
+                return False, "        Response data does not contain \"%s\"" % (item,)
+        for item in notcontains:
+            if respdata.find(item.replace("\n", "\r\n")) != -1:
+                return False, "        Response data incorrectly contains \"%s\"" % (item,)
+
+        return True, ""
+            
