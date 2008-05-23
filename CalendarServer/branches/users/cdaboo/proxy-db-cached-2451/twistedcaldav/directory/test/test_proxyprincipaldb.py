164e74a06cc46837699812a3e3076ab85fbbd55e
cdaboo@apple.com <cdaboo@apple.com@e27351fd-9f3e-4f54-a53b-843176b1656c>
2008-05-23 16:56:31 +0000

    Not sure why all this was commented out. We really need the test though.
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver@2456 e27351fd-9f3e-4f54-a53b-843176b1656c

--- CalendarServer/branches/users/cdaboo/proxy-db-cached-2451/twistedcaldav/directory/test/test_proxyprincipaldb.py
+++ CalendarServer/branches/users/cdaboo/proxy-db-cached-2451/twistedcaldav/directory/test/test_proxyprincipaldb.py
@@ -19,155 +19,155 @@ import os
 
 import twistedcaldav.test.util
 
-#class ProxyPrincipalDB (twistedcaldav.test.util.TestCase):
-#    """
-#    Directory service provisioned principals.
-#    """
-#    
-#    class old_CalendarUserProxyDatabase(CalendarUserProxyDatabase):
-#        
-#        def _db_version(self):
-#            """
-#            @return: the schema version assigned to this index.
-#            """
-#            return "3"
-#            
-#        def _db_init_data_tables(self, q):
-#            """
-#            Initialise the underlying database tables.
-#            @param q:           a database cursor to use.
-#            """
-#    
-#            #
-#            # GROUPS table
-#            #
-#            q.execute(
-#                """
-#                create table GROUPS (
-#                    GROUPNAME   text,
-#                    MEMBER      text
-#                )
-#                """
-#            )
-#
-#    class new_CalendarUserProxyDatabase(CalendarUserProxyDatabase):
-#        
-#        def _db_version(self):
-#            """
-#            @return: the schema version assigned to this index.
-#            """
-#            return "11"
-#            
-#    class newer_CalendarUserProxyDatabase(CalendarUserProxyDatabase):
-#        
-#        def _db_version(self):
-#            """
-#            @return: the schema version assigned to this index.
-#            """
-#            return "51"
-#            
-#    def test_normalDB(self):
-#        """
-#        DirectoryPrincipalResource.groupMembers()
-#        """
-#    
-#        # Get the DB
-#        db_path = self.mktemp()
-#        os.mkdir(db_path)
-#        db = CalendarUserProxyDatabase(db_path)
-#        db.setGroupMembers("A", ("B", "C", "D",))
-#        self.assertEqual(db.getMembers("A"), set(("B", "C", "D",)))
-#        self.assertEqual(db.getMemberships("B"), set(("A",)))
-#
-#    def test_DBIndexed(self):
-#        """
-#        DirectoryPrincipalResource.groupMembers()
-#        """
-#    
-#        # Get the DB
-#        db_path = self.mktemp()
-#        os.mkdir(db_path)
-#        db = CalendarUserProxyDatabase(db_path)
-#        self.assertEqual(set([row[1] for row in db._db_execute("PRAGMA index_list(GROUPS)")]), set(("GROUPNAMES", "MEMBERS")))
-#
-#    def test_OldDB(self):
-#        """
-#        DirectoryPrincipalResource.groupMembers()
-#        """
-#    
-#        # Get the DB
-#        db_path = self.mktemp()
-#        os.mkdir(db_path)
-#        db = self.old_CalendarUserProxyDatabase(db_path)
-#        self.assertEqual(set([row[1] for row in db._db_execute("PRAGMA index_list(GROUPS)")]), set())
-#
-#    def test_DBUpgrade(self):
-#        """
-#        DirectoryPrincipalResource.groupMembers()
-#        """
-#    
-#        # Get the DB
-#        db_path = self.mktemp()
-#        os.mkdir(db_path)
-#        db = self.old_CalendarUserProxyDatabase(db_path)
-#        db.setGroupMembers("A", ("B", "C", "D",))
-#        self.assertEqual(db.getMembers("A"), set(("B", "C", "D",)))
-#        self.assertEqual(db.getMemberships("B"), set(("A",)))
-#        self.assertEqual(set([row[1] for row in db._db_execute("PRAGMA index_list(GROUPS)")]), set())
-#        db._db_close()
-#        db = None
-#        
-#        db = CalendarUserProxyDatabase(db_path)
-#        self.assertEqual(db.getMembers("A"), set(("B", "C", "D",)))
-#        self.assertEqual(db.getMemberships("B"), set(("A",)))
-#        self.assertEqual(set([row[1] for row in db._db_execute("PRAGMA index_list(GROUPS)")]), set(("GROUPNAMES", "MEMBERS")))
-#        db._db_close()
-#        db = None
-#
-#    def test_DBUpgradeNewer(self):
-#        """
-#        DirectoryPrincipalResource.groupMembers()
-#        """
-#    
-#        # Get the DB
-#        db_path = self.mktemp()
-#        os.mkdir(db_path)
-#        db = self.old_CalendarUserProxyDatabase(db_path)
-#        db.setGroupMembers("A", ("B", "C", "D",))
-#        self.assertEqual(db.getMembers("A"), set(("B", "C", "D",)))
-#        self.assertEqual(db.getMemberships("B"), set(("A",)))
-#        self.assertEqual(set([row[1] for row in db._db_execute("PRAGMA index_list(GROUPS)")]), set())
-#        db._db_close()
-#        db = None
-#        
-#        db = self.new_CalendarUserProxyDatabase(db_path)
-#        self.assertEqual(db.getMembers("A"), set(("B", "C", "D",)))
-#        self.assertEqual(db.getMemberships("B"), set(("A",)))
-#        self.assertEqual(set([row[1] for row in db._db_execute("PRAGMA index_list(GROUPS)")]), set(("GROUPNAMES", "MEMBERS")))
-#        db._db_close()
-#        db = None
-#
-#    def test_DBNoUpgradeNewer(self):
-#        """
-#        DirectoryPrincipalResource.groupMembers()
-#        """
-#    
-#        # Get the DB
-#        db_path = self.mktemp()
-#        os.mkdir(db_path)
-#        db = self.new_CalendarUserProxyDatabase(db_path)
-#        db.setGroupMembers("A", ("B", "C", "D",))
-#        self.assertEqual(db.getMembers("A"), set(("B", "C", "D",)))
-#        self.assertEqual(db.getMemberships("B"), set(("A",)))
-#        self.assertEqual(set([row[1] for row in db._db_execute("PRAGMA index_list(GROUPS)")]), set(("GROUPNAMES", "MEMBERS")))
-#        db._db_close()
-#        db = None
-#        
-#        db = self.newer_CalendarUserProxyDatabase(db_path)
-#        self.assertEqual(db.getMembers("A"), set(("B", "C", "D",)))
-#        self.assertEqual(db.getMemberships("B"), set(("A",)))
-#        self.assertEqual(set([row[1] for row in db._db_execute("PRAGMA index_list(GROUPS)")]), set(("GROUPNAMES", "MEMBERS")))
-#        db._db_close()
-#        db = None
+class ProxyPrincipalDB (twistedcaldav.test.util.TestCase):
+    """
+    Directory service provisioned principals.
+    """
+    
+    class old_CalendarUserProxyDatabase(CalendarUserProxyDatabase):
+        
+        def _db_version(self):
+            """
+            @return: the schema version assigned to this index.
+            """
+            return "3"
+            
+        def _db_init_data_tables(self, q):
+            """
+            Initialise the underlying database tables.
+            @param q:           a database cursor to use.
+            """
+    
+            #
+            # GROUPS table
+            #
+            q.execute(
+                """
+                create table GROUPS (
+                    GROUPNAME   text,
+                    MEMBER      text
+                )
+                """
+            )
+
+    class new_CalendarUserProxyDatabase(CalendarUserProxyDatabase):
+        
+        def _db_version(self):
+            """
+            @return: the schema version assigned to this index.
+            """
+            return "11"
+            
+    class newer_CalendarUserProxyDatabase(CalendarUserProxyDatabase):
+        
+        def _db_version(self):
+            """
+            @return: the schema version assigned to this index.
+            """
+            return "51"
+            
+    def test_normalDB(self):
+        """
+        DirectoryPrincipalResource.groupMembers()
+        """
+    
+        # Get the DB
+        db_path = self.mktemp()
+        os.mkdir(db_path)
+        db = CalendarUserProxyDatabase(db_path)
+        db.setGroupMembers("A", ("B", "C", "D",))
+        self.assertEqual(db.getMembers("A"), set(("B", "C", "D",)))
+        self.assertEqual(db.getMemberships("B"), set(("A",)))
+
+    def test_DBIndexed(self):
+        """
+        DirectoryPrincipalResource.groupMembers()
+        """
+    
+        # Get the DB
+        db_path = self.mktemp()
+        os.mkdir(db_path)
+        db = CalendarUserProxyDatabase(db_path)
+        self.assertEqual(set([row[1] for row in db._db_execute("PRAGMA index_list(GROUPS)")]), set(("GROUPNAMES", "MEMBERS")))
+
+    def test_OldDB(self):
+        """
+        DirectoryPrincipalResource.groupMembers()
+        """
+    
+        # Get the DB
+        db_path = self.mktemp()
+        os.mkdir(db_path)
+        db = self.old_CalendarUserProxyDatabase(db_path)
+        self.assertEqual(set([row[1] for row in db._db_execute("PRAGMA index_list(GROUPS)")]), set())
+
+    def test_DBUpgrade(self):
+        """
+        DirectoryPrincipalResource.groupMembers()
+        """
+    
+        # Get the DB
+        db_path = self.mktemp()
+        os.mkdir(db_path)
+        db = self.old_CalendarUserProxyDatabase(db_path)
+        db.setGroupMembers("A", ("B", "C", "D",))
+        self.assertEqual(db.getMembers("A"), set(("B", "C", "D",)))
+        self.assertEqual(db.getMemberships("B"), set(("A",)))
+        self.assertEqual(set([row[1] for row in db._db_execute("PRAGMA index_list(GROUPS)")]), set())
+        db._db_close()
+        db = None
+        
+        db = CalendarUserProxyDatabase(db_path)
+        self.assertEqual(db.getMembers("A"), set(("B", "C", "D",)))
+        self.assertEqual(db.getMemberships("B"), set(("A",)))
+        self.assertEqual(set([row[1] for row in db._db_execute("PRAGMA index_list(GROUPS)")]), set(("GROUPNAMES", "MEMBERS")))
+        db._db_close()
+        db = None
+
+    def test_DBUpgradeNewer(self):
+        """
+        DirectoryPrincipalResource.groupMembers()
+        """
+    
+        # Get the DB
+        db_path = self.mktemp()
+        os.mkdir(db_path)
+        db = self.old_CalendarUserProxyDatabase(db_path)
+        db.setGroupMembers("A", ("B", "C", "D",))
+        self.assertEqual(db.getMembers("A"), set(("B", "C", "D",)))
+        self.assertEqual(db.getMemberships("B"), set(("A",)))
+        self.assertEqual(set([row[1] for row in db._db_execute("PRAGMA index_list(GROUPS)")]), set())
+        db._db_close()
+        db = None
+        
+        db = self.new_CalendarUserProxyDatabase(db_path)
+        self.assertEqual(db.getMembers("A"), set(("B", "C", "D",)))
+        self.assertEqual(db.getMemberships("B"), set(("A",)))
+        self.assertEqual(set([row[1] for row in db._db_execute("PRAGMA index_list(GROUPS)")]), set(("GROUPNAMES", "MEMBERS")))
+        db._db_close()
+        db = None
+
+    def test_DBNoUpgradeNewer(self):
+        """
+        DirectoryPrincipalResource.groupMembers()
+        """
+    
+        # Get the DB
+        db_path = self.mktemp()
+        os.mkdir(db_path)
+        db = self.new_CalendarUserProxyDatabase(db_path)
+        db.setGroupMembers("A", ("B", "C", "D",))
+        self.assertEqual(db.getMembers("A"), set(("B", "C", "D",)))
+        self.assertEqual(db.getMemberships("B"), set(("A",)))
+        self.assertEqual(set([row[1] for row in db._db_execute("PRAGMA index_list(GROUPS)")]), set(("GROUPNAMES", "MEMBERS")))
+        db._db_close()
+        db = None
+        
+        db = self.newer_CalendarUserProxyDatabase(db_path)
+        self.assertEqual(db.getMembers("A"), set(("B", "C", "D",)))
+        self.assertEqual(db.getMemberships("B"), set(("A",)))
+        self.assertEqual(set([row[1] for row in db._db_execute("PRAGMA index_list(GROUPS)")]), set(("GROUPNAMES", "MEMBERS")))
+        db._db_close()
+        db = None
 
         
