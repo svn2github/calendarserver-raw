563cb4b866dae67722adc548fd643523c4eb42e1
dreid@apple.com <dreid@apple.com@e27351fd-9f3e-4f54-a53b-843176b1656c>
2008-06-18 18:38:58 +0000

    Make sure the setup.py does a thing with the kqreactor module.
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver@2594 e27351fd-9f3e-4f54-a53b-843176b1656c

--- CalendarServer/branches/kqueuereactor/setup.py
+++ CalendarServer/branches/kqueuereactor/setup.py
@@ -133,7 +133,7 @@ dist = setup(
                          "twistedcaldav.query", 
                          "twistedcaldav.admin",
                          "twistedcaldav.py", 
-                         "twisted"
+                         "twisted",
                        ],
     package_data     = {
                          "twisted": ["plugins/caldav.py"],
@@ -142,6 +142,7 @@ dist = setup(
     scripts          = [ "bin/caldavd", "bin/caladmin" ],
     data_files       = [ ("caldavd", ["conf/caldavd.plist"]) ],
     ext_modules      = extensions,
+    py_modules       = ["kqreactor"],
 )
 
 if "install" in dist.commands:
