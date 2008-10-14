1b92ff49e216b3050d067353052f0b4ddf2cfb25
sagen@apple.com <sagen@apple.com@e27351fd-9f3e-4f54-a53b-843176b1656c>
2008-10-14 22:46:06 +0000

    Branch for adding wiki directory type
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver@3153 e27351fd-9f3e-4f54-a53b-843176b1656c

---
+++ CalendarServer/branches/users/sagen/wikitype-3152/twistedcaldav/directory/calendar.py
@@ -0,0 +1,441 @@
+##
+# Copyright (c) 2006-2007 Apple Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##
+
+"""
+Implements a directory-backed calendar hierarchy.
+"""
+
+__all__ = [
+    "uidsResourceName",
+   #"DirectoryCalendarProvisioningResource",
+    "DirectoryCalendarHomeProvisioningResource",
+    "DirectoryCalendarHomeTypeProvisioningResource",
+    "DirectoryCalendarHomeUIDProvisioningResource",
+    "DirectoryCalendarHomeResource",
+]
+
+from twisted.internet.defer import succeed
+from twisted.web2 import responsecode
+from twisted.web2.dav import davxml
+from twisted.web2.http import HTTPError
+from twisted.web2.dav.util import joinURL
+from twisted.web2.dav.resource import TwistedACLInheritable, TwistedQuotaRootProperty
+
+from twistedcaldav import caldavxml
+from twistedcaldav.config import config
+from twistedcaldav.dropbox import DropBoxHomeResource
+from twistedcaldav.extensions import ReadOnlyResourceMixIn, DAVResource
+from twistedcaldav.freebusyurl import FreeBusyURLResource
+from twistedcaldav.resource import CalDAVResource
+from twistedcaldav.schedule import ScheduleInboxResource, ScheduleOutboxResource
+from twistedcaldav.directory.idirectory import IDirectoryService
+from twistedcaldav.directory.resource import AutoProvisioningResourceMixIn
+
+# Use __underbars__ convention to avoid conflicts with directory resource types.
+uidsResourceName = "__uids__"
+
+
+class DirectoryCalendarProvisioningResource (
+    AutoProvisioningResourceMixIn,
+    ReadOnlyResourceMixIn,
+    DAVResource,
+):
+    def defaultAccessControlList(self):
+        return config.ProvisioningResourceACL
+
+    def accessControlList(self, request, inheritance=True, expanding=False, inherited_aces=None):
+        # Permissions here are fixed, and are not subject to inherritance rules, etc.
+        return succeed(self.defaultAccessControlList())
+
+
+class DirectoryCalendarHomeProvisioningResource (DirectoryCalendarProvisioningResource):
+    """
+    Resource which provisions calendar home collections as needed.    
+    """
+    def __init__(self, directory, url):
+        """
+        @param directory: an L{IDirectoryService} to provision calendars from.
+        @param url: the canonical URL for the resource.
+        """
+        assert directory is not None
+        assert url.endswith("/"), "Collection URL must end in '/'"
+
+        DAVResource.__init__(self)
+
+        self.directory = IDirectoryService(directory)
+        self._url = url
+
+        # FIXME: Smells like a hack
+        directory.calendarHomesCollection = self
+
+        #
+        # Create children
+        #
+        def provisionChild(name):
+            self.putChild(name, self.provisionChild(name))
+
+        for recordType in self.directory.recordTypes():
+            provisionChild(recordType)
+
+        provisionChild(uidsResourceName)
+
+    def provisionChild(self, recordType):
+        raise NotImplementedError("Subclass must implement provisionChild()")
+
+    def url(self):
+        return self._url
+
+    def getChild(self, name):
+        return self.putChildren.get(name, None)
+
+    def listChildren(self):
+        return self.directory.recordTypes()
+
+    def principalCollections(self):
+        # FIXME: directory.principalCollection smells like a hack
+        # See DirectoryPrincipalProvisioningResource.__init__()
+        return self.directory.principalCollection.principalCollections()
+
+    def principalForRecord(self, record):
+        # FIXME: directory.principalCollection smells like a hack
+        # See DirectoryPrincipalProvisioningResource.__init__()
+        return self.directory.principalCollection.principalForRecord(record)
+
+    def homeForDirectoryRecord(self, record):
+        uidResource = self.getChild(uidsResourceName)
+        if uidResource is None:
+            return None
+        else:
+            return uidResource.getChild(record.guid)
+
+    ##
+    # DAV
+    ##
+    
+    def isCollection(self):
+        return True
+
+
+class DirectoryCalendarHomeTypeProvisioningResource (DirectoryCalendarProvisioningResource):
+    """
+    Resource which provisions calendar home collections of a specific
+    record type as needed.
+    """
+    def __init__(self, parent, recordType):
+        """
+        @param parent: the parent of this resource
+        @param recordType: the directory record type to provision.
+        """
+        assert parent is not None
+        assert recordType is not None
+
+        DAVResource.__init__(self)
+
+        self.directory = parent.directory
+        self.recordType = recordType
+        self._parent = parent
+
+    def url(self):
+        return joinURL(self._parent.url(), self.recordType)
+
+    def getChild(self, name, record=None):
+        self.provision()
+        if name == "":
+            return self
+
+        if record is None:
+            record = self.directory.recordWithShortName(self.recordType, name)
+            if record is None:
+                return None
+
+        return self._parent.homeForDirectoryRecord(record)
+
+    def listChildren(self):
+        if config.EnablePrincipalListings:
+            return (
+                record.shortName
+                for record in self.directory.listRecords(self.recordType)
+                if record.enabledForCalendaring
+            )
+        else:
+            # Not a listable collection
+            raise HTTPError(responsecode.FORBIDDEN)
+
+    def createSimilarFile(self, path):
+        raise HTTPError(responsecode.NOT_FOUND)
+
+    ##
+    # DAV
+    ##
+    
+    def isCollection(self):
+        return True
+
+    ##
+    # ACL
+    ##
+
+    def principalCollections(self):
+        return self._parent.principalCollections()
+
+    def principalForRecord(self, record):
+        return self._parent.principalForRecord(record)
+
+
+class DirectoryCalendarHomeUIDProvisioningResource (DirectoryCalendarProvisioningResource):
+    def __init__(self, parent):
+        """
+        @param parent: the parent of this resource
+        """
+        assert parent is not None
+
+        DAVResource.__init__(self)
+
+        self.directory = parent.directory
+        self.parent = parent
+
+    def url(self):
+        return joinURL(self.parent.url(), uidsResourceName)
+
+    def getChild(self, name, record=None):
+        self.provision()
+        if name == "":
+            return self
+
+        if record is None:
+            record = self.directory.recordWithGUID(name)
+            if record is None:
+                return None
+
+        return self.provisionChild(name)
+
+    def listChildren(self):
+        # Not a listable collection
+        raise HTTPError(responsecode.FORBIDDEN)
+
+    ##
+    # DAV
+    ##
+    
+    def isCollection(self):
+        return True
+
+    ##
+    # ACL
+    ##
+
+    def principalCollections(self):
+        return self.parent.principalCollections()
+
+    def principalForRecord(self, record):
+        return self.parent.principalForRecord(record)
+
+
+class DirectoryCalendarHomeResource (AutoProvisioningResourceMixIn, CalDAVResource):
+    """
+    Calendar home collection resource.
+    """
+    def __init__(self, parent, record):
+        """
+        @param path: the path to the file which will back the resource.
+        """
+        assert parent is not None
+        assert record is not None
+
+        CalDAVResource.__init__(self)
+
+        self.record = record
+        self.parent = parent
+
+        # Cache children which must be of a specific type
+        childlist = (
+            ("inbox" , ScheduleInboxResource ),
+            ("outbox", ScheduleOutboxResource),
+        )
+        if config.EnableDropBox:
+            childlist += (
+                ("dropbox", DropBoxHomeResource),
+            )
+        if config.FreeBusyURL["Enabled"]:
+            childlist += (
+                ("freebusy", FreeBusyURLResource),
+            )
+        for name, cls in childlist:
+            child = self.provisionChild(name)
+            assert isinstance(child, cls), "Child %r is not a %s: %r" % (name, cls.__name__, child)
+            self.putChild(name, child)
+
+    def provisionDefaultCalendars(self):
+
+        # Disable notifications during provisioning
+        if hasattr(self, "clientNotifier"):
+            self.clientNotifier.disableNotify()
+
+        def setupFreeBusy(_):
+            # Default calendar is initially opaque to freebusy
+            child.writeDeadProperty(caldavxml.ScheduleCalendarTransp(caldavxml.Opaque()))
+
+            # FIXME: Shouldn't have to call provision() on another resource
+            # We cheat here because while inbox will auto-provision itself when located,
+            # we need to write a dead property to it pre-emptively.
+            # This will go away once we remove the free-busy-set property on inbox.
+
+            # Set calendar-free-busy-set on inbox
+            inbox = self.getChild("inbox")
+            inbox.provision()
+            inbox.processFreeBusyCalendar(childURL, True)
+
+            # Default calendar may need to be marked as the default for scheduling
+            if config.Scheduling["CalDAV"]["DefaultCalendarProvisioned"]:
+                inbox.writeDeadProperty(caldavxml.ScheduleDefaultCalendarURL(davxml.HRef(childURL)))
+
+            return self
+
+        try:
+            self.provision()
+
+            childName = "calendar"
+            childURL = joinURL(self.url(), childName)
+            child = self.provisionChild(childName)
+            assert isinstance(child, CalDAVResource), "Child %r is not a %s: %r" % (childName, CalDAVResource.__name__, child)
+
+            d = child.createCalendarCollection()
+            d.addCallback(setupFreeBusy)
+        except:
+            # We want to make sure to re-enable notifications, so do so
+            # if there is an immediate exception above, or via errback, below
+            if hasattr(self, "clientNotifier"):
+                self.clientNotifier.enableNotify(None)
+            raise
+
+        # Re-enable notifications
+        if hasattr(self, "clientNotifier"):
+            d.addCallback(self.clientNotifier.enableNotify)
+            d.addErrback(self.clientNotifier.enableNotify)
+
+        return d
+
+    def provisionChild(self, name):
+        raise NotImplementedError("Subclass must implement provisionChild()")
+
+    def url(self):
+        return joinURL(self.parent.url(), self.record.guid)
+        ##
+        ## While the underlying primary location is GUID-based, we want
+        ## the canonical user-facing location to be recordType &
+        ## shortName-based, because that's friendlier.
+        ##
+        #return joinURL(self.parent.parent.getChild(self.record.recordType).url(), self.record.shortName)
+
+    ##
+    # DAV
+    ##
+    
+    def isCollection(self):
+        return True
+
+    ##
+    # ACL
+    ##
+
+    def owner(self, request):
+        return succeed(davxml.HRef(self.principalForRecord().principalURL()))
+
+    def ownerPrincipal(self, request):
+        return succeed(self.principalForRecord())
+
+    def defaultAccessControlList(self):
+        myPrincipal = self.principalForRecord()
+
+        aces = (
+            # DAV:read access for authenticated users.
+            davxml.ACE(
+                davxml.Principal(davxml.Authenticated()),
+                davxml.Grant(
+                    davxml.Privilege(davxml.Read()),
+                    davxml.Privilege(davxml.ReadCurrentUserPrivilegeSet()),
+                ),
+            ),
+            # Inheritable DAV:all access for the resource's associated principal.
+            davxml.ACE(
+                davxml.Principal(davxml.HRef(myPrincipal.principalURL())),
+                davxml.Grant(davxml.Privilege(davxml.All())),
+                davxml.Protected(),
+                TwistedACLInheritable(),
+            ),
+            # Inheritable CALDAV:read-free-busy access for authenticated users.
+            davxml.ACE(
+                davxml.Principal(davxml.Authenticated()),
+                davxml.Grant(davxml.Privilege(caldavxml.ReadFreeBusy())),
+                TwistedACLInheritable(),
+            ),
+        )
+
+        # Give read access to config.ReadPrincipals
+        aces += config.ReadACEs
+
+        # Give all access to config.AdminPrincipals
+        aces += config.AdminACEs
+        
+        if config.EnableProxyPrincipals:
+            aces += (
+                # DAV:read access for this principal's calendar-proxy-read users.
+                davxml.ACE(
+                    davxml.Principal(davxml.HRef(joinURL(myPrincipal.principalURL(), "calendar-proxy-read/"))),
+                    davxml.Grant(davxml.Privilege(davxml.Read())),
+                    davxml.Protected(),
+                    TwistedACLInheritable(),
+                ),
+                # DAV:read/DAV:write access for this principal's calendar-proxy-write users.
+                davxml.ACE(
+                    davxml.Principal(davxml.HRef(joinURL(myPrincipal.principalURL(), "calendar-proxy-write/"))),
+                    davxml.Grant(davxml.Privilege(davxml.Read()), davxml.Privilege(davxml.Write())),
+                    davxml.Protected(),
+                    TwistedACLInheritable(),
+                ),
+            )
+
+        return davxml.ACL(*aces)
+
+    def accessControlList(self, request, inheritance=True, expanding=False, inherited_aces=None):
+        # Permissions here are fixed, and are not subject to inherritance rules, etc.
+        return succeed(self.defaultAccessControlList())
+
+    def principalCollections(self):
+        return self.parent.principalCollections()
+
+    def principalForRecord(self):
+        return self.parent.principalForRecord(self.record)
+
+    ##
+    # Quota
+    ##
+
+    def hasQuotaRoot(self, request):
+        """
+        @return: a C{True} if this resource has quota root, C{False} otherwise.
+        """
+        return self.hasDeadProperty(TwistedQuotaRootProperty) or config.UserQuota
+    
+    def quotaRoot(self, request):
+        """
+        @return: a C{int} containing the maximum allowed bytes if this collection
+            is quota-controlled, or C{None} if not quota controlled.
+        """
+        if self.hasDeadProperty(TwistedQuotaRootProperty):
+            return int(str(self.readDeadProperty(TwistedQuotaRootProperty)))
+        else:
+            return config.UserQuota
---
+++ CalendarServer/branches/users/sagen/wikitype-3152/twistedcaldav/query/calendarquery.py
@@ -0,0 +1,300 @@
+##
+# Copyright (c) 2006-2007 Apple Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##
+
+"""
+Convert a calendar-query into an expression tree.
+Convert a calendar-query into a partial SQL statement.
+"""
+
+__version__ = "0.0"
+
+__all__ = [
+    "calendarquery",
+    "sqlcalendarquery",
+]
+
+from twistedcaldav.dateops import floatoffset
+from twistedcaldav.query import sqlgenerator
+from twistedcaldav.query import expression
+from twistedcaldav import caldavxml
+
+# SQL Index column (field) names
+
+FIELD_TYPE = "RESOURCE.TYPE"
+FIELD_UID  = "RESOURCE.UID"
+#FIELD_RECURRENCE_ID = "RESOURCE.RECURRENCE-ID"
+#FIELD_SUMMARY = "RESOURCE.SUMMARY"
+#FIELD_DESCRIPTION = "RESOURCE.DESCRIPTION"
+#FIELD_LOCATION = "RESOURCE.LOCATION"
+
+def calendarquery(filter):
+    """
+    Convert the supplied calendar-query into an expression tree.
+
+    @param filter: the L{Filter} for the calendar-query to convert.
+    @return: a L{baseExpression} for the expression tree.
+    """
+    
+    # Lets assume we have a valid filter from the outset.
+    
+    # Top-level filter contains exactly one comp-filter element
+    assert len(filter.children) == 1
+    vcalfilter = filter.children[0]
+    assert isinstance(vcalfilter, caldavxml.ComponentFilter)
+    assert vcalfilter.filter_name == "VCALENDAR"
+    
+    if len(vcalfilter.children) > 0:
+        # Only comp-filters are handled
+        for _ignore in [x for x in vcalfilter.children if not isinstance(x, caldavxml.ComponentFilter)]:
+            raise ValueError
+        
+        return compfilterListExpression(vcalfilter.children)
+    else:
+        return expression.allExpression()
+
+def compfilterListExpression(compfilters):
+    """
+    Create an expression for a list of comp-filter elements.
+    
+    @param compfilters: the C{list} of L{ComponentFilter} elements.
+    @return: a L{baseExpression} for the expression tree.
+    """
+    
+    if len(compfilters) == 1:
+        return compfilterExpression(compfilters[0])
+    else:
+        return expression.orExpression([compfilterExpression(c) for c in compfilters])
+
+def compfilterExpression(compfilter):
+    """
+    Create an expression for a single comp-filter element.
+    
+    @param compfilter: the L{ComponentFilter} element.
+    @return: a L{baseExpression} for the expression tree.
+    """
+    
+    # Handle is-not-defined case
+    if not compfilter.defined:
+        # Test for TYPE != <<component-type name>>
+        return expression.isnotExpression(FIELD_TYPE, compfilter.filter_name, True)
+        
+    expressions = []
+    if isinstance(compfilter.filter_name, str):
+        expressions.append(expression.isExpression(FIELD_TYPE, compfilter.filter_name, True))
+    else:
+        expressions.append(expression.inExpression(FIELD_TYPE, compfilter.filter_name, True))
+    
+    # Handle time-range    
+    if compfilter.qualifier and isinstance(compfilter.qualifier, caldavxml.TimeRange):
+        start, end, startfloat, endfloat = getTimerangeArguments(compfilter.qualifier)
+        expressions.append(expression.timerangeExpression(start, end, startfloat, endfloat))
+        
+    # Handle properties - we can only do UID right now
+    props = []
+    for p in [x for x in compfilter.filters if isinstance(x, caldavxml.PropertyFilter)]:
+        props.append(propfilterExpression(p))
+    if len(props) > 1:
+        propsExpression = expression.orExpression[props]
+    elif len(props) == 1:
+        propsExpression = props[0]
+    else:
+        propsExpression = None
+        
+    # Handle embedded components - we do not right now as our Index does not handle them
+    comps = []
+    for _ignore in [x for x in compfilter.filters if isinstance(x, caldavxml.ComponentFilter)]:
+        raise ValueError
+    if len(comps) > 1:
+        compsExpression = expression.orExpression[comps]
+    elif len(comps) == 1:
+        compsExpression = comps[0]
+    else:
+        compsExpression = None
+
+    # Now build compound expression
+    if ((propsExpression is not None) and (compsExpression is not None)):
+        expressions.append(expression.orExpression([propsExpression, compsExpression]))
+    elif propsExpression is not None:
+        expressions.append(propsExpression)
+    elif compsExpression is not None:
+        expressions.append(compsExpression)
+
+    # Now build return expression
+    return expression.andExpression(expressions)
+
+def propfilterExpression(propfilter):
+    """
+    Create an expression for a single prop-filter element.
+    
+    @param propfilter: the L{PropertyFilter} element.
+    @return: a L{baseExpression} for the expression tree.
+    """
+    
+    # Only handle UID right now
+    if propfilter.filter_name != "UID":
+        raise ValueError
+
+    # Handle is-not-defined case
+    if not propfilter.defined:
+        # Test for <<field>> != "*"
+        return expression.isExpression(FIELD_UID, "", True)
+    
+    # Handle time-range - we cannot do this with our Index right now
+    if propfilter.qualifier and isinstance(propfilter.qualifier, caldavxml.TimeRange):
+        raise ValueError
+    
+    # Handle text-match
+    tm = None
+    if propfilter.qualifier and isinstance(propfilter.qualifier, caldavxml.TextMatch):
+        if propfilter.qualifier.negate:
+            tm = expression.notcontainsExpression(propfilter.filter_name, str(propfilter.qualifier), propfilter.qualifier)
+        else:
+            tm = expression.containsExpression(propfilter.filter_name, str(propfilter.qualifier), propfilter.qualifier)
+    
+    # Handle embedded parameters - we do not right now as our Index does not handle them
+    params = []
+    for _ignore in propfilter.filters:
+        raise ValueError
+    if len(params) > 1:
+        paramsExpression = expression.orExpression[params]
+    elif len(params) == 1:
+        paramsExpression = params[0]
+    else:
+        paramsExpression = None
+
+    # Now build return expression
+    if (tm is not None) and (paramsExpression is not None):
+        return expression.andExpression([tm, paramsExpression])
+    elif tm is not None:
+        return tm
+    elif paramsExpression is not None:
+        return paramsExpression
+    else:
+        return None
+
+def getTimerangeArguments(timerange):
+    """
+    Get start/end and floating start/end (adjusted for timezone offset) values from the
+    supplied time-range test.
+    
+    @param timerange: the L{TimeRange} used in the query.
+    @return: C{tuple} of C{str} for start, end, startfloat, endfloat
+    """
+    
+    # Start/end in UTC
+    start = timerange.start
+    end = timerange.end
+    
+    # Get timezone
+    tzinfo = timerange.tzinfo
+
+    # Now force to floating UTC
+    startfloat = floatoffset(start, tzinfo)
+    endfloat = floatoffset(end, tzinfo)
+
+    return str(start), str(end), str(startfloat), str(endfloat)
+
+def sqlcalendarquery(filter):
+    """
+    Convert the supplied calendar-query into a oartial SQL statement.
+
+    @param filter: the L{Filter} for thw calendar-query to convert.
+    @return: a C{tuple} of (C{str}, C{list}), where the C{str} is the partial SQL statement,
+            and the C{list} is the list of argument substitutions to use with the SQL API execute method.
+            Or return C{None} if it is not possible to create an SQL query to fully match the calendar-query.
+    """
+    try:
+        expression = calendarquery(filter)
+        sql = sqlgenerator.sqlgenerator(expression)
+        return sql.generate()
+    except ValueError:
+        return None
+
+
+if __name__ == "__main__":
+    import datetime
+
+    filter = caldavxml.Filter(
+                 caldavxml.ComponentFilter(
+                     *[caldavxml.ComponentFilter(
+                           *[caldavxml.TimeRange(**{"start":"20060605T160000Z", "end":"20060605T170000Z"})],
+                           **{"name":("VEVENT", "VFREEBUSY", "VAVAILABILITY")}
+                       )],
+                     **{"name":"VCALENDAR"}
+                 )
+             )
+
+    # A complete implementation of current DST rules for major US time zones.
+    
+    def first_sunday_on_or_after(dt):
+        days_to_go = 6 - dt.weekday()
+        if days_to_go:
+            dt += datetime.timedelta(days_to_go)
+        return dt
+    
+    # In the US, DST starts at 2am (standard time) on the first Sunday in April.
+    DSTSTART = datetime.datetime(1, 4, 1, 2)
+    # and ends at 2am (DST time; 1am standard time) on the last Sunday of Oct.
+    # which is the first Sunday on or after Oct 25.
+    DSTEND = datetime.datetime(1, 10, 25, 1)
+    
+    ZERO = datetime.timedelta(0)
+    HOUR = datetime.timedelta(hours=1)
+
+    class USTimeZone(datetime.tzinfo):
+    
+        def __init__(self, hours, reprname, stdname, dstname):
+            self.stdoffset = datetime.timedelta(hours=hours)
+            self.reprname = reprname
+            self.stdname = stdname
+            self.dstname = dstname
+    
+        def __repr__(self):
+            return self.reprname
+    
+        def tzname(self, dt):
+            if self.dst(dt):
+                return self.dstname
+            else:
+                return self.stdname
+    
+        def utcoffset(self, dt):
+            return self.stdoffset + self.dst(dt)
+    
+        def dst(self, dt):
+            if dt is None or dt.tzinfo is None:
+                # An exception may be sensible here, in one or both cases.
+                # It depends on how you want to treat them.  The default
+                # fromutc() implementation (called by the default astimezone()
+                # implementation) passes a datetime with dt.tzinfo is self.
+                return ZERO
+            assert dt.tzinfo is self
+    
+            # Find first Sunday in April & the last in October.
+            start = first_sunday_on_or_after(DSTSTART.replace(year=dt.year))
+            end = first_sunday_on_or_after(DSTEND.replace(year=dt.year))
+    
+            # Can't compare naive to aware objects, so strip the timezone from
+            # dt first.
+            if start <= dt.replace(tzinfo=None) < end:
+                return HOUR
+            else:
+                return ZERO
+
+    Eastern  = USTimeZone(-5, "Eastern",  "EST", "EDT")
+    filter.children[0].settzinfo(Eastern)
+    
+    print sqlcalendarquery(filter)
