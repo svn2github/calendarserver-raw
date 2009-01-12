b866e585e7d8b691e3f81d6a52b0e47ecd8cb550
cdaboo@apple.com <cdaboo@apple.com@e27351fd-9f3e-4f54-a53b-843176b1656c>
2009-01-12 20:11:05 +0000

    Working on multi-realm support.
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver@3591 e27351fd-9f3e-4f54-a53b-843176b1656c

---
+++ CalendarServer/branches/users/cdaboo/multiple-realms-3590/twistedcaldav/test/test_memcacher.py
@@ -0,0 +1,112 @@
+# Copyright (c) 2007 Twisted Matrix Laboratories.
+# See LICENSE for details.
+
+"""
+Test the memcacher cache abstraction.
+"""
+
+from twisted.internet.defer import inlineCallbacks
+
+from twistedcaldav.config import config
+from twistedcaldav.memcacher import Memcacher
+from twistedcaldav.test.util import TestCase
+
+class MemcacherTestCase(TestCase):
+    """
+    Test Memcacher abstract cache.
+    """
+
+    @inlineCallbacks
+    def test_setget(self):
+
+        for processType in ("Single", "Combined",):
+            config.ProcessType = processType
+
+            cacher = Memcacher("testing")
+    
+            result = yield cacher.set("akey", "avalue")
+            self.assertTrue(result)
+
+            result = yield cacher.get("akey")
+            if isinstance(cacher._memcacheProtocol, Memcacher.nullCacher):
+                self.assertEquals(None, result)
+            else:
+                self.assertEquals("avalue", result)
+
+    @inlineCallbacks
+    def test_missingget(self):
+
+        for processType in ("Single", "Combined",):
+            config.ProcessType = processType
+
+            cacher = Memcacher("testing")
+    
+            result = yield cacher.get("akey")
+            self.assertEquals(None, result)
+
+    @inlineCallbacks
+    def test_delete(self):
+
+        for processType in ("Single", "Combined",):
+            config.ProcessType = processType
+
+            cacher = Memcacher("testing")
+    
+            result = yield cacher.set("akey", "avalue")
+            self.assertTrue(result)
+    
+            result = yield cacher.get("akey")
+            if isinstance(cacher._memcacheProtocol, Memcacher.nullCacher):
+                self.assertEquals(None, result)
+            else:
+                self.assertEquals("avalue", result)
+    
+            result = yield cacher.delete("akey")
+            self.assertTrue(result)
+    
+            result = yield cacher.get("akey")
+            self.assertEquals(None, result)
+
+    @inlineCallbacks
+    def test_all_pickled(self):
+
+        for processType in ("Single", "Combined",):
+            config.ProcessType = processType
+
+            cacher = Memcacher("testing", pickle=True)
+    
+            result = yield cacher.set("akey", ["1", "2", "3",])
+            self.assertTrue(result)
+    
+            result = yield cacher.get("akey")
+            if isinstance(cacher._memcacheProtocol, Memcacher.nullCacher):
+                self.assertEquals(None, result)
+            else:
+                self.assertEquals(["1", "2", "3",], result)
+    
+            result = yield cacher.delete("akey")
+            self.assertTrue(result)
+    
+            result = yield cacher.get("akey")
+            self.assertEquals(None, result)
+
+    @inlineCallbacks
+    def test_all_noinvalidation(self):
+
+        for processType in ("Single", "Combined",):
+            config.ProcessType = processType
+
+            cacher = Memcacher("testing", no_invalidation=True)
+    
+            result = yield cacher.set("akey", ["1", "2", "3",])
+            self.assertTrue(result)
+    
+            result = yield cacher.get("akey")
+            self.assertEquals(["1", "2", "3",], result)
+    
+            result = yield cacher.delete("akey")
+            self.assertTrue(result)
+    
+            result = yield cacher.get("akey")
+            self.assertEquals(None, result)
+
