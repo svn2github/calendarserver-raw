757c81783159af42b4162064de302f9966571ddc
sagen@apple.com <sagen@apple.com@e27351fd-9f3e-4f54-a53b-843176b1656c>
2009-06-29 22:43:02 +0000

    Branch for working on processing of inbox items remaining after a snowleo upgrade
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver@4390 e27351fd-9f3e-4f54-a53b-843176b1656c

---
+++ CalendarServer/branches/users/sagen/pending-invites-4389/twistedcaldav/__init__.py
@@ -0,0 +1,63 @@
+##
+# Copyright (c) 2005-2007 Apple Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##
+
+"""
+WebDAV support for Twisted Web2.
+
+See draft spec: http://ietf.webdav.org/caldav/draft-dusseault-caldav.txt
+"""
+
+#
+# Set __version__
+#
+
+try:
+    from twistedcaldav.version import version as __version__
+except ImportError:
+    __version__ = None
+
+#
+# Load in suitable file extension/content-type map from OS X
+#
+
+from twisted.web2.static import File, loadMimeTypes
+
+File.contentTypes = loadMimeTypes(("/etc/apache2/mime.types", "/etc/httpd/mime.types",))
+
+#
+# Register additional WebDAV XML elements
+#
+
+import twisted.web2.dav.davxml
+import twistedcaldav.caldavxml
+import twistedcaldav.customxml
+
+twisted.web2.dav.davxml.registerElements(twistedcaldav.caldavxml)
+twisted.web2.dav.davxml.registerElements(twistedcaldav.customxml)
+
+#
+# DefaultHTTPHandler
+#
+
+from twisted.web2.http_headers import DefaultHTTPHandler, last, singleHeader
+
+DefaultHTTPHandler.updateParsers({
+    "If-Schedule-Tag-Match": (last, str),
+})
+DefaultHTTPHandler.updateGenerators({
+    "Schedule-Tag": (str, singleHeader),
+})
+
