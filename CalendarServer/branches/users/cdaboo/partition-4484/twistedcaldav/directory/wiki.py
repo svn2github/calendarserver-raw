e9f73e218ba2cec33a39b6f40063cbabaea04774
cdaboo@apple.com <cdaboo@apple.com@e27351fd-9f3e-4f54-a53b-843176b1656c>
2009-08-06 18:51:47 +0000

    git-svn-id: https://svn.calendarserver.org/repository/calendarserver@4485 e27351fd-9f3e-4f54-a53b-843176b1656c

---
+++ CalendarServer/branches/users/cdaboo/partition-4484/twistedcaldav/directory/wiki.py
@@ -0,0 +1,264 @@
+##
+# Copyright (c) 2006-2009 Apple Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##
+
+"""
+Directory service implementation for users who are allowed to authorize
+as other principals.
+"""
+
+__all__ = [
+    "WikiDirectoryService",
+]
+
+from twisted.web2.dav import davxml
+from twisted.web.xmlrpc import Proxy, Fault
+from twisted.web2.http import HTTPError, StatusResponse
+from twisted.web2.auth.wrapper import UnauthorizedResponse
+
+from twisted.internet.defer import inlineCallbacks, returnValue
+
+
+from twisted.web2.dav.resource import TwistedACLInheritable
+from twistedcaldav.config import config
+from twistedcaldav.directory.directory import (DirectoryService,
+                                               DirectoryRecord,
+                                               UnknownRecordTypeError)
+from twistedcaldav.log import Logger
+
+log = Logger()
+
+class WikiDirectoryService(DirectoryService):
+    """
+    L{IDirectoryService} implementation for Wikis.
+    """
+    baseGUID = "D79EF1E0-9A42-11DD-AD8B-0800200C9A66"
+
+    realmName = None
+
+    recordType_wikis = "wikis"
+
+    UIDPrefix = "wiki-"
+
+
+    def __repr__(self):
+        return "<%s %r>" % (self.__class__.__name__, self.realmName)
+
+    def __init__(self):
+        super(WikiDirectoryService, self).__init__()
+        self.byUID = {}
+        self.byShortName = {}
+
+    def recordTypes(self):
+        return (WikiDirectoryService.recordType_wikis,)
+
+    def listRecords(self, recordType):
+        return ()
+
+    def recordWithShortName(self, recordType, shortName):
+        if recordType != WikiDirectoryService.recordType_wikis:
+            raise UnknownRecordTypeError(recordType)
+
+        if self.byShortName.has_key(shortName):
+            record = self.byShortName[shortName]
+            self.log_info("Returning existing wiki record with UID %s" %
+                (record.uid,))
+            return record
+
+        record = self._addRecord(shortName)
+        return record
+
+    def recordWithUID(self, uid):
+
+        if self.byUID.has_key(uid):
+            record = self.byUID[uid]
+            self.log_info("Returning existing wiki record with UID %s" %
+                (record.uid,))
+            return record
+
+        if uid.startswith(self.UIDPrefix):
+            shortName = uid[len(self.UIDPrefix):]
+            record = self._addRecord(shortName)
+            return record
+        else:
+            return None
+
+    def _addRecord(self, shortName):
+
+        record = WikiDirectoryRecord(
+            self,
+            WikiDirectoryService.recordType_wikis,
+            shortName,
+            None
+        )
+        self.log_info("Creating wiki record with GUID %s" % (record.guid,))
+        self.byUID[record.uid] = record
+        self.byShortName[shortName] = record
+        return record
+
+
+class WikiDirectoryRecord(DirectoryRecord):
+    """
+    L{DirectoryRecord} implementation for Wikis.
+    """
+
+    def __init__(self, service, recordType, shortName, entry):
+        super(WikiDirectoryRecord, self).__init__(
+            service=service,
+            recordType=recordType,
+            guid=None,
+            shortNames=(shortName,),
+            fullName=shortName,
+            enabledForCalendaring=True,
+            uid="%s%s" % (WikiDirectoryService.UIDPrefix, shortName),
+        )
+
+
+
+@inlineCallbacks
+def getWikiACL(resource, request):
+    """
+    Ask the wiki server we're paired with what level of access the authnUser has.
+
+    Returns an ACL.
+
+    Wiki authentication is a bit tricky because the end-user accessing a group
+    calendar may not actually be enabled for calendaring.  Therefore in that
+    situation, the authzUser will have been replaced with the wiki principal
+    in locateChild( ), so that any changes the user makes will have the wiki
+    as the originator.  The authnUser will always be the end-user.
+    """
+    from twistedcaldav.directory.principal import DirectoryPrincipalResource
+
+    if (not hasattr(resource, "record") or
+        resource.record.recordType != WikiDirectoryService.recordType_wikis):
+        returnValue(None)
+
+    if hasattr(request, 'wikiACL'):
+        returnValue(request.wikiACL)
+
+    wikiConfig = config.Authentication.Wiki
+    userID = "unauthenticated"
+    wikiID = resource.record.shortNames[0]
+
+    try:
+        url = str(request.authnUser.children[0])
+        principal = (yield request.locateResource(url))
+        if isinstance(principal, DirectoryPrincipalResource):
+            userID = principal.record.guid
+    except:
+        # TODO: better error handling
+        pass
+
+    proxy = Proxy(wikiConfig["URL"])
+    try:
+
+        log.debug("Looking up Wiki ACL for: user [%s], wiki [%s]" % (userID,
+            wikiID))
+        access = (yield proxy.callRemote(wikiConfig["WikiMethod"],
+            userID, wikiID))
+
+        log.debug("Wiki ACL result: user [%s], wiki [%s], access [%s]" % (userID,
+            wikiID, access))
+
+        # The ACL we returns has ACEs for the end-user and the wiki principal
+        # in case authzUser is the wiki principal.
+        if access == "read":
+            request.wikiACL =   davxml.ACL(
+                                    davxml.ACE(
+                                        request.authnUser,
+                                        davxml.Grant(
+                                            davxml.Privilege(davxml.Read()),
+                                        ),
+                                        TwistedACLInheritable(),
+                                    ),
+                                    davxml.ACE(
+                                        davxml.Principal(
+                                            davxml.HRef.fromString("/principals/wikis/%s/" % (wikiID,))
+                                        ),
+                                        davxml.Grant(
+                                            davxml.Privilege(davxml.Read()),
+                                        ),
+                                        TwistedACLInheritable(),
+                                    )
+                                )
+            returnValue(request.wikiACL)
+
+        elif access in ("write", "admin"):
+            request.wikiACL =   davxml.ACL(
+                                    davxml.ACE(
+                                        request.authnUser,
+                                        davxml.Grant(
+                                            davxml.Privilege(davxml.Read()),
+                                        ),
+                                        TwistedACLInheritable(),
+                                    ),
+                                    davxml.ACE(
+                                        request.authnUser,
+                                        davxml.Grant(
+                                            davxml.Privilege(davxml.Write()),
+                                        ),
+                                        TwistedACLInheritable(),
+                                    ),
+                                    davxml.ACE(
+                                        davxml.Principal(
+                                            davxml.HRef.fromString("/principals/wikis/%s/" % (wikiID,))
+                                        ),
+                                        davxml.Grant(
+                                            davxml.Privilege(davxml.Read()),
+                                        ),
+                                        TwistedACLInheritable(),
+                                    ),
+                                    davxml.ACE(
+                                        davxml.Principal(
+                                            davxml.HRef.fromString("/principals/wikis/%s/" % (wikiID,))
+                                        ),
+                                        davxml.Grant(
+                                            davxml.Privilege(davxml.Write()),
+                                        ),
+                                        TwistedACLInheritable(),
+                                    )
+                                )
+            returnValue(request.wikiACL)
+
+        else: # "no-access":
+
+            if userID == "unauthenticated":
+                # Return a 401 so they have an opportunity to log in
+                response = (yield UnauthorizedResponse.makeResponse(
+                    request.credentialFactories,
+                    request.remoteAddr,
+                ))
+                raise HTTPError(response)
+
+            raise HTTPError(
+                StatusResponse(
+                    403,
+                    "You are not allowed to access this wiki"
+                )
+            )
+
+
+    except Fault, fault:
+
+        log.debug("Wiki ACL result: user [%s], wiki [%s], FAULT [%s]" % (userID,
+            wikiID, fault))
+
+        if fault.faultCode == 2: # non-existent user
+            raise HTTPError(StatusResponse(403, fault.faultString))
+
+        else: # fault.faultCode == 12, non-existent wiki
+            raise HTTPError(StatusResponse(404, fault.faultString))
+
---
+++ CalendarServer/branches/users/cdaboo/partition-4484/twistedcaldav/memcacheprops.py
@@ -0,0 +1,333 @@
+##
+# Copyright (c) 2009 Apple Computer, Inc. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+##
+
+"""
+DAV Property store using memcache on top of another property store
+implementation.
+"""
+
+__all__ = ["MemcachePropertyCollection"]
+
+try:
+    from hashlib import md5
+except ImportError:
+    from md5 import new as md5
+
+from memcacheclient import ClientFactory as MemcacheClientFactory, MemcacheError, TokenMismatchError
+
+from twisted.python.filepath import FilePath
+from twisted.web2 import responsecode
+from twisted.web2.http import HTTPError, StatusResponse
+
+from twistedcaldav.config import config
+from twistedcaldav.log import LoggingMixIn, Logger
+
+log = Logger()
+
+NoValue = ""
+
+
+class MemcachePropertyCollection (LoggingMixIn):
+    """
+    Manages a single property store for all resources in a collection.
+    """
+    def __init__(self, collection, cacheTimeout=0):
+        self.collection = collection
+        self.cacheTimeout = cacheTimeout
+
+    @classmethod
+    def memcacheClient(cls, refresh=False):
+        if not hasattr(MemcachePropertyCollection, "_memcacheClient"):
+
+            log.info("Instantiating memcache connection for MemcachePropertyCollection")
+
+            MemcachePropertyCollection._memcacheClient = MemcacheClientFactory.getClient(["%s:%s" % (config.Memcached.BindAddress, config.Memcached.Port)],
+                debug=0,
+                pickleProtocol=2,
+            )
+            assert MemcachePropertyCollection._memcacheClient is not None
+
+        return MemcachePropertyCollection._memcacheClient
+
+    def propertyCache(self):
+        # The property cache has this format:
+        #  {
+        #    "/path/to/resource/file":
+        #      (
+        #        {
+        #          (namespace, name): property,
+        #          ...,
+        #        },
+        #        memcache_token,
+        #      ),
+        #    ...,
+        #  }
+        if not hasattr(self, "_propertyCache"):
+            self._propertyCache = self._loadCache()
+        return self._propertyCache
+
+    def childCache(self, child):
+        path = child.fp.path
+        key = self._keyForPath(path)
+        propertyCache = self.propertyCache()
+
+        try:
+            childCache, token = propertyCache[key]
+        except KeyError:
+            self.log_debug("No child property cache for %s" % (child,))
+            childCache, token = ({}, None)
+
+            #message = "No child property cache for %s" % (child,)
+            #log.error(message)
+            #raise AssertionError(message)
+
+        return propertyCache, key, childCache, token
+
+    def _keyForPath(self, path):
+        key = "|".join((
+            self.__class__.__name__,
+            path
+        ))
+        return md5(key).hexdigest()
+
+    def _loadCache(self, childNames=None):
+        if childNames is None:
+            abortIfMissing = False
+            childNames = self.collection.listChildren()
+        else:
+            if childNames:
+                abortIfMissing = True
+            else:
+                return {}
+
+        self.log_debug("Loading cache for %s" % (self.collection,))
+
+        client = self.memcacheClient()
+        assert client is not None, "OMG no cache!"
+        if client is None:
+            return None
+
+        keys = tuple((
+            (self._keyForPath(self.collection.fp.child(childName).path), childName)
+            for childName in childNames
+        ))
+
+        result = client.gets_multi((key for key, name in keys))
+
+        if self.logger.willLogAtLevel("debug"):
+            if abortIfMissing:
+                missing = "missing "
+            else:
+                missing = ""
+            self.log_debug("Loaded keys for %schildren of %s: %s" % (
+                missing,
+                self.collection,
+                [name for key, name in keys],
+            ))
+
+        missing = tuple((
+            name for key, name in keys
+            if key not in result
+        ))
+
+        if missing:
+            if abortIfMissing:
+                raise MemcacheError("Unable to fully load cache for %s" % (self.collection,))
+
+            loaded = self._buildCache(childNames=missing)
+            loaded = self._loadCache(childNames=(FilePath(name).basename() for name in loaded.iterkeys()))
+
+            result.update(loaded.iteritems())
+
+        return result
+
+    def _storeCache(self, cache):
+        self.log_debug("Storing cache for %s" % (self.collection,))
+
+        values = dict((
+            (self._keyForPath(path), props)
+            for path, props
+            in cache.iteritems()
+        ))
+
+        client = self.memcacheClient()
+        if client is not None:
+            client.set_multi(values, time=self.cacheTimeout)
+
+    def _buildCache(self, childNames=None):
+        if childNames is None:
+            childNames = self.collection.listChildren()
+        elif not childNames:
+            return {}
+
+        self.log_debug("Building cache for %s" % (self.collection,))
+
+        cache = {}
+        ds = []
+        for childName in childNames:
+            d = self.collection.getChild(childName)
+            def _gotChild(child):
+                if child is not None:
+                    propertyStore = child.deadProperties()
+                    props = {}
+                    for qname in propertyStore.list(cache=False):
+                        props[qname] = propertyStore.get(qname, cache=False)
+
+                    cache[child.fp.path] = props
+            d.addCallback(_gotChild)
+            ds.append(d)
+        def _collectedChildren(_):
+            self._storeCache(cache)
+            return cache
+        return DeferredList(ds).addCallback(_collectedChildren)
+
+
+    def setProperty(self, child, property, delete=False):
+        propertyCache, key, childCache, token = self.childCache(child)
+
+        if delete:
+            qname = property
+            if childCache.has_key(qname):
+                del childCache[qname]
+        else:
+            qname = property.qname()
+            childCache[qname] = property
+
+        client = self.memcacheClient()
+
+        if client is not None:
+            retries = 10
+            while retries:
+                try:
+                    if client.set(key, childCache, time=self.cacheTimeout,
+                        token=token):
+                        # Success
+                        break
+
+                except TokenMismatchError:
+                    # The value in memcache has changed since we last
+                    # fetched it
+                    log.debug("memcacheprops setProperty TokenMismatchError; retrying...")
+
+                finally:
+                    # Re-fetch the properties for this child
+                    loaded = self._loadCache(childNames=(child.fp.basename(),))
+                    propertyCache.update(loaded.iteritems())
+
+                retries -= 1
+
+                propertyCache, key, childCache, token = self.childCache(child)
+
+                if delete:
+                    if childCache.has_key(qname):
+                        del childCache[qname]
+                else:
+                    childCache[qname] = property
+
+            else:
+                log.error("memcacheprops setProperty had too many failures")
+                delattr(self, "_propertyCache")
+                raise MemcacheError("Unable to %s property {%s}%s on %s"
+                    % ("delete" if delete else "set",
+                    qname[0], qname[1], child))
+
+    def deleteProperty(self, child, qname):
+        return self.setProperty(child, qname, delete=True)
+
+    def flushCache(self, child):
+        path = child.fp.path
+        key = self._keyForPath(path)
+        propertyCache = self.propertyCache()
+
+        if key in propertyCache:
+            del propertyCache[key]
+
+        client = self.memcacheClient()
+        if client is not None:
+            result = client.delete(key)
+            if not result:
+                raise MemcacheError("Unable to flush cache on %s" % (child,))
+
+    def propertyStoreForChild(self, child, childPropertyStore):
+        return self.ChildPropertyStore(self, child, childPropertyStore)
+
+    class ChildPropertyStore (LoggingMixIn):
+        def __init__(self, parentPropertyCollection, child, childPropertyStore):
+            self.parentPropertyCollection = parentPropertyCollection
+            self.child = child
+            self.childPropertyStore = childPropertyStore
+
+        def propertyCache(self):
+            path = self.child.fp.path
+            key = self.parentPropertyCollection._keyForPath(path)
+            parentPropertyCache = self.parentPropertyCollection.propertyCache()
+            return parentPropertyCache.get(key, ({}, None))[0]
+
+        def flushCache(self):
+            self.parentPropertyCollection.flushCache(self.child)
+
+        def get(self, qname, cache=True):
+            if cache:
+                propertyCache = self.propertyCache()
+                if qname in propertyCache:
+                    return propertyCache[qname]
+                else:
+                    raise HTTPError(StatusResponse(
+                        responsecode.NOT_FOUND,
+                        "No such property: {%s}%s" % qname
+                    ))
+
+            self.log_debug("Read for %s on %s"
+                           % (qname, self.childPropertyStore.resource.fp.path))
+            return self.childPropertyStore.get(qname)
+
+        def set(self, property):
+            self.log_debug("Write for %s on %s"
+                           % (property.qname(), self.childPropertyStore.resource.fp.path))
+
+            self.parentPropertyCollection.setProperty(self.child, property)
+            self.childPropertyStore.set(property)
+
+        def delete(self, qname):
+            self.log_debug("Delete for %s on %s"
+                           % (qname, self.childPropertyStore.resource.fp.path))
+
+            self.parentPropertyCollection.deleteProperty(self.child, qname)
+            self.childPropertyStore.delete(qname)
+
+        def contains(self, qname, cache=True):
+            if cache:
+                propertyCache = self.propertyCache()
+                return qname in propertyCache
+
+            self.log_debug("Contains for %s"
+                           % (self.childPropertyStore.resource.fp.path,))
+            return self.childPropertyStore.contains(qname)
+
+        def list(self, cache=True):
+            if cache:
+                propertyCache = self.propertyCache()
+                return propertyCache.iterkeys()
+
+            self.log_debug("List for %s"
+                           % (self.childPropertyStore.resource.fp.path,))
+            return self.childPropertyStore.list()
