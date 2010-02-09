bee202ae0721c4502b3d2d5bfd7e944a772c9e22
glyph@apple.com <glyph@apple.com@e27351fd-9f3e-4f54-a53b-843176b1656c>
2010-02-09 21:57:13 +0000

    Creating branch for removing dependence on Twisted patches.
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver@5084 e27351fd-9f3e-4f54-a53b-843176b1656c

---
+++ CalendarServer/branches/users/glyph/use-system-twisted/twistedcaldav/test/test_memcacher.py
@@ -0,0 +1,127 @@
+# Copyright (c) 2007 Twisted Matrix Laboratories.
+# See LICENSE for details.
+
+"""
+Test the memcacher cache abstraction.
+"""
+
+from twisted.internet.defer import inlineCallbacks
+
+from twistedcaldav.config import config
+from twistedcaldav.memcacher import Memcacher
+from twistedcaldav.test.util import TestCase
+
+class MemcacherTestCase(TestCase):
+    """
+    Test Memcacher abstract cache.
+    """
+
+    @inlineCallbacks
+    def test_setget(self):
+
+        for processType in ("Single", "Combined",):
+            config.ProcessType = processType
+
+            cacher = Memcacher("testing")
+    
+            result = yield cacher.set("akey", "avalue")
+            self.assertTrue(result)
+
+            result = yield cacher.get("akey")
+            if isinstance(cacher._memcacheProtocol, Memcacher.nullCacher):
+                self.assertEquals(None, result)
+            else:
+                self.assertEquals("avalue", result)
+
+    @inlineCallbacks
+    def test_missingget(self):
+
+        for processType in ("Single", "Combined",):
+            config.ProcessType = processType
+
+            cacher = Memcacher("testing")
+    
+            result = yield cacher.get("akey")
+            self.assertEquals(None, result)
+
+    @inlineCallbacks
+    def test_delete(self):
+
+        for processType in ("Single", "Combined",):
+            config.ProcessType = processType
+
+            cacher = Memcacher("testing")
+    
+            result = yield cacher.set("akey", "avalue")
+            self.assertTrue(result)
+    
+            result = yield cacher.get("akey")
+            if isinstance(cacher._memcacheProtocol, Memcacher.nullCacher):
+                self.assertEquals(None, result)
+            else:
+                self.assertEquals("avalue", result)
+    
+            result = yield cacher.delete("akey")
+            self.assertTrue(result)
+    
+            result = yield cacher.get("akey")
+            self.assertEquals(None, result)
+
+    @inlineCallbacks
+    def test_all_pickled(self):
+
+        for processType in ("Single", "Combined",):
+            config.ProcessType = processType
+
+            cacher = Memcacher("testing", pickle=True)
+    
+            result = yield cacher.set("akey", ["1", "2", "3",])
+            self.assertTrue(result)
+    
+            result = yield cacher.get("akey")
+            if isinstance(cacher._memcacheProtocol, Memcacher.nullCacher):
+                self.assertEquals(None, result)
+            else:
+                self.assertEquals(["1", "2", "3",], result)
+    
+            result = yield cacher.delete("akey")
+            self.assertTrue(result)
+    
+            result = yield cacher.get("akey")
+            self.assertEquals(None, result)
+
+    @inlineCallbacks
+    def test_all_noinvalidation(self):
+
+        for processType in ("Single", "Combined",):
+            config.ProcessType = processType
+
+            cacher = Memcacher("testing", no_invalidation=True)
+    
+            result = yield cacher.set("akey", ["1", "2", "3",])
+            self.assertTrue(result)
+    
+            result = yield cacher.get("akey")
+            self.assertEquals(["1", "2", "3",], result)
+    
+            result = yield cacher.delete("akey")
+            self.assertTrue(result)
+    
+            result = yield cacher.get("akey")
+            self.assertEquals(None, result)
+
+    def test_keynormalization(self):
+
+        for processType in ("Single", "Combined",):
+            config.ProcessType = processType
+
+            cacher = Memcacher("testing")
+            
+            self.assertTrue(len(cacher._normalizeKey("A" * 100)) <= 250)
+            self.assertTrue(len(cacher._normalizeKey("A" * 512)) <= 250)
+            
+            key = cacher._normalizeKey(" \n\t\r" * 20)
+            self.assertTrue(" " not in key)
+            self.assertTrue("\n" not in key)
+            self.assertTrue("\t" not in key)
+            self.assertTrue("\r" not in key)
