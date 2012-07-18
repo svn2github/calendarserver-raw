svn2github/calendarserver-raw

commit 2a2da3542ec30a334bb547ac61b26e4c19cfe607

Include number of ischedule requests in access log entries.
git-svn-id: https://svn.calendarserver.org/repository/calendarserver@9422 e27351fd-9f3e-4f54-a53b-843176b1656c

CalendarServer/trunk/twistedcaldav/scheduling/ischedule.py
@@ -173,6 +173,12 @@ class IScheduleRequest(object):
         
         # Generate an HTTP client request
         try:
+            if not hasattr(self.scheduler.request, "extendedLogItems"):
+                self.request.extendedLogItems = {}
+            if "itip.ischedule" not in self.request.extendedLogItems:
+                self.request.extendedLogItems["itip.ischedule"] = 0
+            self.request.extendedLogItems["itip.ischedule"] += 1
+
             from twisted.internet import reactor
             f = Factory()
             f.protocol = HTTPClientProtocol

commit bc6e6478efedd97c3facad32e51e4ed59500cc35

Fix server-to-server bug introduced in r9422
git-svn-id: https://svn.calendarserver.org/repository/calendarserver@9462 e27351fd-9f3e-4f54-a53b-843176b1656c

CalendarServer/trunk/twistedcaldav/scheduling/ischedule.py
@@ -174,10 +174,10 @@ class IScheduleRequest(object):
         # Generate an HTTP client request
         try:
             if not hasattr(self.scheduler.request, "extendedLogItems"):
-                self.request.extendedLogItems = {}
-            if "itip.ischedule" not in self.request.extendedLogItems:
-                self.request.extendedLogItems["itip.ischedule"] = 0
-            self.request.extendedLogItems["itip.ischedule"] += 1
+                self.scheduler.request.extendedLogItems = {}
+            if "itip.ischedule" not in self.scheduler.request.extendedLogItems:
+                self.scheduler.request.extendedLogItems["itip.ischedule"] = 0
+            self.scheduler.request.extendedLogItems["itip.ischedule"] += 1
 
             from twisted.internet import reactor
             f = Factory()