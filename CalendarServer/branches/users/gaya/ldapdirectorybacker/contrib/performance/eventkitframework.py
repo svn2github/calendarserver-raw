2ee9ce140f7b6923ea1b210766867ccfe2086760
gaya@apple.com <gaya@apple.com@e27351fd-9f3e-4f54-a53b-843176b1656c>
2012-09-26 21:12:46 +0000

    obsoleted by directorybacker branch
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver@9863 e27351fd-9f3e-4f54-a53b-843176b1656c

--- CalendarServer/branches/users/gaya/ldapdirectorybacker/contrib/performance/eventkitframework.py
+++
@@ -1,8 +0,0 @@
-import objc as _objc
-
-__bundle__ = _objc.initFrameworkWrapper("EventKit",
-    frameworkIdentifier="com.apple.EventKit",
-    frameworkPath=_objc.pathForFramework(
-    "/System/Library/Frameworks/EventKit.framework"),
-    globals=globals())
-
