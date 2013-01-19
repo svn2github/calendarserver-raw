9b519b99ab44c78dc2ee139aa5b94b25c5b056a5
gaya@apple.com <gaya@apple.com@e27351fd-9f3e-4f54-a53b-843176b1656c>
2013-01-19 04:44:01 +0000

    update from trunk
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver@10313 e27351fd-9f3e-4f54-a53b-843176b1656c

--- CalDAVTester/branches/users/gaya/sharedgroupstester/src/caldavtest.py
+++ CalDAVTester/branches/users/gaya/sharedgroupstester/src/caldavtest.py
@@ -1,5 +1,5 @@
 ##
-# Copyright (c) 2006-2012 Apple Inc. All rights reserved.
+# Copyright (c) 2006-2013 Apple Inc. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -342,20 +342,23 @@ class caldavtest(object):
                         else:
                             status = False
 
-                        # Get properties for this propstat
-                        prop = props.findall("{DAV:}prop")
-                        for el in prop:
-
+                        if status:
                             # Get properties for this propstat
-                            glm = el.findall("{DAV:}getlastmodified")
-                            if len(glm) != 1:
-                                continue
-                            value = glm[0].text
-                            value = rfc822.parsedate(value)
-                            value = time.mktime(value)
-                            if value > latest:
-                                hresult = href
-                                latest = value
+                            prop = props.findall("{DAV:}prop")
+                            for el in prop:
+
+                                # Get properties for this propstat
+                                glm = el.findall("{DAV:}getlastmodified")
+                                if len(glm) != 1:
+                                    continue
+                                value = glm[0].text
+                                value = rfc822.parsedate(value)
+                                value = time.mktime(value)
+                                if value > latest:
+                                    hresult = href
+                                    latest = value
+                        elif not hresult:
+                            hresult = href
 
         return hresult
 
@@ -490,6 +493,14 @@ class caldavtest(object):
             self.grabbedlocation = self.dofindnew(collection, label=label)
             req.method = "GET"
             req.ruri = "$"
+
+        # Special for FINDNEW
+        elif req.method == "FINDNEW":
+            collection = (req.ruri, req.user, req.pswd)
+            self.grabbedlocation = self.dofindnew(collection, label=label)
+            if req.graburi:
+                self.manager.server_info.addextrasubs({req.graburi: self.grabbedlocation})
+            return True, "", None, None
 
         # Special check for WAITCOUNT
         elif req.method.startswith("WAITCOUNT"):
