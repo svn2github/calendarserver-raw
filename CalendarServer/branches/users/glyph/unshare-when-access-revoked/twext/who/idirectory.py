6dc4b5f07b12e9a0e1858eae6fb4b47a024a2500
glyph@apple.com <glyph@apple.com@e27351fd-9f3e-4f54-a53b-843176b1656c>
2013-01-29 03:27:34 +0000

    Create branch for un-sharing automatically when external access controls are removed.
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver@10562 e27351fd-9f3e-4f54-a53b-843176b1656c

---
+++ CalendarServer/branches/users/glyph/unshare-when-access-revoked/twext/who/idirectory.py
@@ -0,0 +1,150 @@
+##
+# Copyright (c) 2006-2013 Apple Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##
+
+"""
+Directory service interface.
+"""
+
+__all__ = [
+    "DirectoryServiceError",
+    "QueryNotSupportedError",
+    "FieldName",
+    "MatchType",
+    "Operand",
+    "QueryFlags",
+    "DirectoryQueryMatchExpression",
+    "IDirectoryService",
+    "IDirectoryRecord",
+]
+
+from zope.interface import Attribute, Interface
+
+from twisted.python.constants import Names, NamedConstant
+from twisted.python.constants import Flags, FlagConstant
+
+
+
+##
+# Exceptions
+##
+
+class DirectoryServiceError(RuntimeError):
+    """
+    Directory service generic error.
+    """
+
+class QueryNotSupportedError(DirectoryServiceError):
+    """
+    Query not supported.
+    """
+
+
+
+##
+# Data Types
+##
+
+class RecordType(Names):
+    user  = NamedConstant()
+    group = NamedConstant()
+
+class FieldName(Names):
+    """
+    Constants for common field names.
+    """
+    recordType     = NamedConstant()
+    shortNames     = NamedConstant()
+    uid            = NamedConstant()
+    guid           = NamedConstant()
+    emailAddresses = NamedConstant()
+
+    shortNames.multiValue     = True
+    emailAddresses.multiValue = True
+
+    @classmethod
+    def isMultiValue(cls, name):
+        return hasattr(name, "multiValue") and name.multiValue
+
+class MatchType(Names):
+    """
+    Query match types.
+    """
+    equals     = NamedConstant()
+    startsWith = NamedConstant()
+    contains   = NamedConstant()
+
+class Operand(Names):
+    OR  = NamedConstant()
+    AND = NamedConstant()
+
+class QueryFlags(Flags):
+    """
+    Query flags.
+    """
+    caseInsensitive = FlagConstant()
+
+class DirectoryQueryMatchExpression(object):
+    """
+    Directory query.
+
+    @ivar fieldName: a L{FieldName}
+    @ivar fieldValue: a text value to match
+    @ivar matchType: a L{MatchType}
+    @ivar flags: l{QueryFlags}
+    """
+
+    def __init__(self, fieldName, fieldValue, matchType=MatchType.equals, flags=None):
+        self.fieldName  = fieldName
+        self.fieldValue = fieldValue
+        self.matchType  = matchType
+        self.flags      = flags
+
+
+
+##
+# Interfaces
+##
+
+class IDirectoryService(Interface):
+    """
+    Directory service.
+    """
+    realmName = Attribute("The name of the authentication realm this service represents.")
+
+    def recordTypes():
+        """
+        @return: a deferred iterable of strings denoting the record
+            types that are kept in the directory.  For example:
+            C{("users", "groups", "resources")}.
+        """
+
+    def recordFromQuery(expressions, operand=Operand.AND):
+        """
+        @return: a deferred iterable of matching L{IDirectoryRecord}s.
+        @raises: L{QueryNotSupportedError} is the query is not
+            supported by this directory service.
+        """
+
+
+
+class IDirectoryRecord(Interface):
+    """
+    Directory record.
+
+    Fields may also be accessed as attributes of L{IDirectoryRecord} objects.
+    """
+    service = Attribute("The L{IDirectoryService} this record exists in.")
+    fields  = Attribute("A dictionary with L{FieldName} keys and text values.")
