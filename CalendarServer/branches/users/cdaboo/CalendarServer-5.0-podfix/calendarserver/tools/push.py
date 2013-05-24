c3a5c72d97a6258dab6c8ff54e2df8d41c3d64b7
cdaboo@apple.com <cdaboo@apple.com@e27351fd-9f3e-4f54-a53b-843176b1656c>
2013-05-24 20:34:56 +0000

    Branch for cross-pod iSchedule fixes.
    git-svn-id: https://svn.calendarserver.org/repository/calendarserver@11247 e27351fd-9f3e-4f54-a53b-843176b1656c

---
+++ CalendarServer/branches/users/cdaboo/CalendarServer-5.0-podfix/calendarserver/tools/push.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python
+##
+# Copyright (c) 2012-2013 Apple Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##
+from __future__ import print_function
+
+from calendarserver.tap.util import getRootResource
+from calendarserver.tools.cmdline import utilityMain
+from errno import ENOENT, EACCES
+from argparse import ArgumentParser
+from twext.python.log import Logger
+from twisted.application.service import Service
+from twisted.internet import reactor
+from twisted.internet.defer import inlineCallbacks
+from twistedcaldav.config import config, ConfigurationError
+import sys
+import time
+
+log = Logger()
+
+
+class WorkerService(Service):
+
+    def __init__(self, store):
+        self._store = store
+
+
+    def rootResource(self):
+        try:
+            rootResource = getRootResource(config, self._store)
+        except OSError, e:
+            if e.errno == ENOENT:
+                # Trying to re-write resources.xml but its parent directory does
+                # not exist.  The server's never been started, so we're missing
+                # state required to do any work.  (Plus, what would be the point
+                # of purging stuff from a server that's completely empty?)
+                raise ConfigurationError(
+                    "It appears that the server has never been started.\n"
+                    "Please start it at least once before purging anything.")
+            elif e.errno == EACCES:
+                # Trying to re-write resources.xml but it is not writable by the
+                # current user.  This most likely means we're in a system
+                # configuration and the user doesn't have sufficient privileges
+                # to do the other things the tool might need to do either.
+                raise ConfigurationError("You must run this tool as root.")
+            else:
+                raise
+        return rootResource
+
+
+    @inlineCallbacks
+    def startService(self):
+        try:
+            yield self.doWork()
+        except ConfigurationError, ce:
+            sys.stderr.write("Error: %s\n" % (str(ce),))
+        except Exception, e:
+            sys.stderr.write("Error: %s\n" % (e,))
+            raise
+        finally:
+            reactor.stop()
+
+
+
+class DisplayAPNSubscriptions(WorkerService):
+
+    users = []
+
+    def doWork(self):
+        rootResource = self.rootResource()
+        directory = rootResource.getDirectory()
+        return displayAPNSubscriptions(self._store, directory, rootResource,
+            self.users)
+
+
+
+def main():
+
+    parser = ArgumentParser(description='Display Apple Push Notification subscriptions')
+    parser.add_argument('-f', '--config', dest='configFileName', metavar='CONFIGFILE', help='caldavd.plist configuration file path')
+    parser.add_argument('-d', '--debug', action='store_true', help='show debug logging')
+    parser.add_argument('user', help='one or more users to display', nargs='+') # Required
+    args = parser.parse_args()
+
+    DisplayAPNSubscriptions.users = args.user
+
+    utilityMain(
+        args.configFileName,
+        DisplayAPNSubscriptions,
+        verbose=args.debug,
+    )
+
+
+
+@inlineCallbacks
+def displayAPNSubscriptions(store, directory, root, users):
+    for user in users:
+        print
+        record = directory.recordWithShortName("users", user)
+        if record is not None:
+            print("User %s (%s)..." % (user, record.uid))
+            txn = store.newTransaction(label="Display APN Subscriptions")
+            subscriptions = (yield txn.apnSubscriptionsBySubscriber(record.uid))
+            (yield txn.commit())
+            if subscriptions:
+                byKey = {}
+                for token, key, timestamp, userAgent, ipAddr in subscriptions:
+                    byKey.setdefault(key, []).append((token, timestamp, userAgent, ipAddr))
+                for key, tokens in byKey.iteritems():
+                    print
+                    protocol, _ignore_host, path = key.strip("/").split("/", 2)
+                    resource = {
+                        "CalDAV" : "calendar",
+                        "CardDAV" : "addressbook",
+                    }[protocol]
+                    if "/" in path:
+                        uid, collection = path.split("/")
+                    else:
+                        uid = path
+                        collection = None
+                    record = directory.recordWithUID(uid)
+                    user = record.shortNames[0]
+                    if collection:
+                        print("...is subscribed to a share from %s's %s home" % (user, resource),)
+                    else:
+                        print("...is subscribed to %s's %s home" % (user, resource),)
+                        # print("   (key: %s)\n" % (key,))
+                    print("with %d device(s):" % (len(tokens),))
+                    for token, timestamp, userAgent, ipAddr in tokens:
+                        print(" %s\n   '%s' from %s\n   %s" % (
+                            token, userAgent, ipAddr,
+                            time.strftime(
+                                "on %a, %d %b %Y at %H:%M:%S %z(%Z)",
+                                time.localtime(timestamp)
+                            )
+                        ))
+            else:
+                print(" ...is not subscribed to anything.")
+        else:
+            print("User %s not found" % (user,))
---
+++ CalendarServer/branches/users/cdaboo/CalendarServer-5.0-podfix/contrib/performance/benchlib.sh
@@ -0,0 +1,111 @@
+##
+# Copyright (c) 2010-2013 Apple Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##
+
+set -e # Break on error
+shopt -s nullglob # Expand foo* to nothing if nothing matches
+
+# Names of database backends that can be benchmarked.
+BACKENDS=(filesystem postgresql)
+
+# Location of the CalendarServer source.  Will automatically be
+# updated to the appropriate version, config edited to use the right
+# backend, and PID files will be discovered beneath it.
+SOURCE=~/Projects/CalendarServer/trunk
+
+# The plist the server will respect.
+CONF=$SOURCE/conf/caldavd-dev.plist
+
+# Names of benchmarks we can run.  Since ordering makes a difference to how
+# benchmarks are split across multiple hosts, new benchmarks should be appended
+# to this list, not inserted earlier on.
+BENCHMARKS="find_calendars find_events event_move event_delete_attendee event_add_attendee event_change_date event_change_summary event_delete vfreebusy event bounded_recurrence unbounded_recurrence event_autoaccept bounded_recurrence_autoaccept unbounded_recurrence_autoaccept vfreebusy_vary_attendees"
+
+# Custom scaling parameters for benchmarks that merit it.  Be careful
+# not to exceed the 99 user limit for benchmarks where the scaling
+# parameter represents a number of users!
+SCALE_PARAMETERS="--parameters find_events:1,10,100,1000,10000 --parameters vfreebusy_vary_attendees:1,9,30"
+
+# Names of metrics we can collect.
+STATISTICS=(HTTP SQL read write pagein pageout)
+
+# Codespeed add-result location.
+ADDURL=http://localhost:8000/result/add/
+
+EXTRACT=$(PWD)/extractconf
+
+# Change the config beneath $SOURCE to use a particular database backend.
+function setbackend() {
+  ./setbackend $SOURCE/conf/caldavd-test.plist $1 > $CONF
+}
+
+# Clean up $SOURCE, update to the specified revision, and build the
+# extensions (in-place).
+function update_and_build() {
+  pushd $SOURCE
+  stop
+  svn st --no-ignore | grep '^[?I]' | cut -c9- | xargs rm -r
+  svn up -r$1 .
+  python setup.py build_ext -i
+  popd
+}
+
+# Ensure that the required configuration file is present, exit if not.
+function check_conf() {
+  if [ ! -e $CONF ]; then
+    echo "Configuration file $CONF is missing."
+    exit 1
+  fi
+}
+
+# Start a CalendarServer in the current directory.  Only return after
+# the specified number of slave processes have written their PID files
+# (which is only a weak metric for "the server is ready to use").
+function start() {
+  NUM_INSTANCES=$1
+  check_conf
+  PIDDIR=$SOURCE/$($EXTRACT $CONF ServerRoot)/$($EXTRACT $CONF RunRoot)
+
+  shift
+  ./run -d $*
+  while sleep 2; do
+    instances=($PIDDIR/*instance*)
+    if [ "${#instances[*]}" -eq "$NUM_INSTANCES" ]; then
+      echo "instance pid files: ${instances[*]}"
+      break
+    fi
+  done
+}
+
+# Stop the CalendarServer in the current directory.  Only return after
+# it has exited.
+function stop() {
+  if [ ! -e $CONF ]; then
+    return
+  fi
+  PIDFILE=$SOURCE/$($EXTRACT $CONF ServerRoot)/$($EXTRACT $CONF RunRoot)/$($EXTRACT $CONF PIDFile)
+  ./run -k || true
+  while :; do
+      pid=$(cat $PIDFILE 2>/dev/null || true)
+      if [ ! -e $PIDFILE ]; then
+	  break
+      fi
+      if ! $(kill -0 $pid); then
+	  break
+      fi
+    echo "Waiting for server to exit..."
+    sleep 1
+  done
+}
